c01f970aef5f2d705b1f9296b41421222da096a7
Pierre Le Guyader <ple-guya@f4r7s14.clusters.42paris.fr>
2025-08-21 12:59:03 +0200

    premiere ebauche, a debugger

---
+++ srcs/mariadb/tools/init_db.sh
@@ -0,0 +1,14 @@
+service mariadb start
+sleep 5
+
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
+
+mariadb -e "CREATE USER IF NOT EXISTS \` ${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \` ${MYSQL_USER}\`@'%';"
+
+mariadb -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+
+mysqldsafe --port=420 --bind-adress=0.0.0.0 --datadir='/var/lib/mysql'
